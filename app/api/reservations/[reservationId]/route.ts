6d2e61095f0fa7dccd08c22518397810ac533497
user <me@example.com>
2023-12-26 18:46:25 +0800

    Minor bug changes

--- app/api/reservations/[reservationId]/route.ts
+++ app/api/reservations/[reservationId]/route.ts
@@ -8,7 +8,7 @@ interface IParams {
 }
 
 export async function DELETE(
-    request: RequestMode,
+    request: Request,
     { params }: { params: IParams }
 ){
     const currentUser = await getCurrentUser();
